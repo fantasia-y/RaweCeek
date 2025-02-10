1003af428dda5c6f257a6980f434df378ebb58c3
Gordon Kirsch <therealfantasia@gmx.de>
2025-02-10 12:19:14 +0100

    Initial Commit

---
+++ RaweCeek/RaweCeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaweCeekApp.swift
+//  RaweCeek
+//
+//  Created by Gordon on 10.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct RaweCeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
